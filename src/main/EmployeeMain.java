95c649bfd4575b65b3570a8df90d58b81331ed31
Vaibhav Diwan <divaibhav@gmail.com>
2020-09-04 12:43:26 +0530

    implemented sorting

--- src/main/EmployeeMain.java
+++ src/main/EmployeeMain.java
@@ -43,14 +43,66 @@ public class EmployeeMain {
             // to deal with \n.
             sc.nextLine();
             employee.setEmail(sc.nextLine());
+            //assigning object to array index
             arr[i] = employee;
 
 
         }
         System.out.println(Arrays.toString(arr));
+        //for sorting we are going to define a method
+        // we then call that method to perform sorting
+        //calling sortSalaryOrder()
+        Employee[] sorted = sortSalaryOrder(arr);
+        System.out.println("sorted array");
+        System.out.println(Arrays.toString(sorted));
+        Employee[] sortedExp = sortExperienceOrder(arr);
+        System.out.println("sorted array by exp");
+        System.out.println(Arrays.toString(sortedExp));
 
 
     }
-    
+
+    //defining method to sort array on the basis of salary, in descending order
+    public static Employee[] sortSalaryOrder(Employee[] arr) {
+        //Employee[] response = arr;// still original will be affected, so we need a copy of this arr. for that we are using clone()
+        Employee[] response = arr.clone();// now we have created clone of original array
+        //to sort we are using bubble sort technique
+        for (int i = 0; i < response.length; i++) { // if response os of size 5
+            for (int j = 0; j < response.length - 1; j++) {
+                //compare elements
+                if (response[j].getSalary() < response[j + 1].getSalary()) {
+                    //swap them
+                    //declaring temporary variable of type employee, because type of array is employee
+                    Employee temp = response[j];
+                    response[j] = response[j + 1];
+                    response[j + 1] = temp;
+                }
+            }
+
+        }
+
+        return response;
+    }
+
+    public static Employee[] sortExperienceOrder(Employee[] arr) {
+        //Employee[] response = arr;// still original will be affected, so we need a copy of this arr. for that we are using clone()
+        Employee[] response = arr.clone();// now we have created clone of original array
+        //to sort we are using bubble sort technique
+        for (int i = 0; i < response.length; i++) { // if response os of size 5
+            for (int j = 0; j < response.length - 1; j++) {
+                //compare elements
+                if (response[j].getExperience() < response[j + 1].getExperience()) {
+                    //swap them
+                    //declaring temporary variable of type employee, because type of array is employee
+                    Employee temp = response[j];
+                    response[j] = response[j + 1];
+                    response[j + 1] = temp;
+                }
+            }
+
+        }
+
+        return response;
+    }
 
 }
